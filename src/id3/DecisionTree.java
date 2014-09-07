a643971f28070184b79c4756907e7ef6460661cc
JuanRahi <rahi.juan@gmail.com>
2014-09-06 23:52:06 -0300

    "Print with colors"

--- src/id3/DecisionTree.java
+++ src/id3/DecisionTree.java
@@ -13,10 +13,8 @@ public class DecisionTree {
 	private void print(Node nodeToPrint, String indent, String value, int level) {
 		String newIndent = indent + "  ";		
 		if(nodeToPrint instanceof ClassNode){
-			ClassNode node = (ClassNode) nodeToPrint;
-			//System.out.println(indent + "-----------------");
+			ClassNode node = (ClassNode) nodeToPrint;			
                         System.out.println("\033[31m" + indent + level + ". "  + value + "="+ node.className);                                                
-                        //System.out.println(indent + "-------------------------");
 		}else{
 			DecisionNode node = (DecisionNode) nodeToPrint;
 			System.out.println(indent + level + ". " + allAttributes[node.attribute] + "->");
@@ -26,29 +24,6 @@ public class DecisionTree {
 			}
 		}
 		
-	}
-
-	/*public String predictTargetAttributeValue(String[] newInstance) {
-		return predict(root, newInstance);
-	}
-
-
-	private String predict(Node currentNode, String[] newInstance) {
-		if(currentNode instanceof ClassNode){
-			ClassNode node = (ClassNode) currentNode;
-			return node.className;
-		}else{
-			DecisionNode node = (DecisionNode) currentNode;
-			String value = newInstance[node.attribute];
-			for(int i=0; i< node.attributeValues.length; i++){
-				if(node.attributeValues[i].equals(value)){
-					return predict(node.nodes[i], newInstance);
-				}
-			}
-		}
-		return null; 
-	}*/
-	
-	
+	}	
 
 }
